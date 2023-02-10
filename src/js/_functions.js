2b780deda4872bb203eb4aaad83f8fb433757e12
raysofgoodness <shady5324@gmail.com>
2023-02-10 20:50:38 +0200

    remove lazyload

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,9 +1,9 @@
 import { burger } from './functions/burger';
 import { animationHeroText } from './functions/animations';
 import { initSlider } from './functions/init-slider';
-import { lazyLoad } from "./functions/lazy-load";
+// import { lazyLoad } from "./functions/lazy-load";
 
 animationHeroText();
 initSlider();
 
-window.addEventListener('load', lazyLoad);
+// window.addEventListener('load', lazyLoad);
